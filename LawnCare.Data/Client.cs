2472f7ef04297987a47a2af8bf2cfd7ad06b147d
charris1083 <camharris1083@gmail.com>
2018-11-29 16:53:28 -0500

    Commit 3

---
+++ LawnCare.Data/Client.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawnCare.Data
+{
+    class Client
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required]
+        public Guid ClientId { get; set; }
+
+        [Required]
+        public string ClientName { get; set; }
+
+        [Required]
+        public string ClientCity { get; set; }
+
+        [Required]
+        public string ClientNeeds { get; set; }
+    }
+}
