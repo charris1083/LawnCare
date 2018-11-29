64d3346366492aa56a5221401774d8d3fb407ffc
charris1083 <camharris1083@gmail.com>
2018-11-29 14:48:37 -0500

    second commit

--- LawnCare.Tests/Controllers/HomeControllerTest.cs
+++ LawnCare.Tests/Controllers/HomeControllerTest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using LawnCare;
-using LawnCare.Controllers;
+using LawnCare.WebMVC.Controllers;
 
 namespace LawnCare.Tests.Controllers
 {
